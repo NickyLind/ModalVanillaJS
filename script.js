b04ce981fa031bc93052575daf6631a0d5e7748e
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-14 16:35:02 -0700

    add ability to close modal with the escape key, and remove active border form buttons

--- script.js
+++ script.js
@@ -8,17 +8,20 @@ const btnOpenModal = document.querySelectorAll('.show-modal');
 const toggleModal = () => {
   modal.classList.toggle('hidden')
   overlay.classList.toggle('hidden')
-}
+};
 
-const removeModalOutsideClick = () => {
+const closeModal = () => {
   modal.classList.add('hidden')
   overlay.classList.add('hidden')  
-}
+};
 
 for(let i = 0; i < btnOpenModal.length; i++) {
   btnOpenModal[i].addEventListener('click', toggleModal);
 };
 
 btnCloseModal.addEventListener('click', toggleModal);
+overlay.addEventListener('click', closeModal);
 
-overlay.addEventListener('click', removeModalOutsideClick)
+document.addEventListener('keydown', function (e) {
+  e.key === "Escape" ? closeModal() : null
+});
