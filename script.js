12414c1abaa241302952664d5655c812731ee821
Nick Lindau <nicholaithegreat@gmail.com>
2021-07-14 16:17:46 -0700

    add functions to toggle modal display by button click and overlay click

--- script.js
+++ script.js
@@ -5,8 +5,20 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnOpenModal);
+const toggleModal = () => {
+  modal.classList.toggle('hidden')
+  overlay.classList.toggle('hidden')
+}
+
+const removeModalOutsideClick = () => {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')  
+}
 
 for(let i = 0; i < btnOpenModal.length; i++) {
-  console.log(btnOpenModal[i].textContent);
-}
+  btnOpenModal[i].addEventListener('click', toggleModal);
+};
+
+btnCloseModal.addEventListener('click', toggleModal);
+
+overlay.addEventListener('click', removeModalOutsideClick)
